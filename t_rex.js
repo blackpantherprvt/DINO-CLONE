905cbab17e6be7e74d9fd6b250883226ebbc324f
Ibrahim fariat <fariatodesigner@gmail.com>
2020-10-26 01:57:31 -0400

    added difficulty based on score

--- t_rex.js
+++ t_rex.js
@@ -9,9 +9,12 @@ let score = document.getElementById('score');
 
 // # -- Public methods -- # //
 let tools = {
-    generateRandomRGBA : 'rgba(' + Math.random() * 365 + ',' + Math.random() * 365 + ',' + Math.random() * 365 + ',1)',
+    generateRandomRGBA: 'rgba(' + Math.floor(Math.random() * 365) + ',' + Math.floor(Math.random() * 365) + ',' + Math.floor(Math.random() * 365) + ',1)',
     randRGBA() {
         return this.generateRandomRGBA;
+    },
+    lerp(start, end, amt) {
+        return (1 - amt) * start + amt * end;
     }
 };
 
@@ -19,13 +22,13 @@ let tools = {
 
 let game_settings = {
     time: 1,
-    score:0,
-    gravity: -0.7,
+    score: '0',
+    gravity: -0.75,
     acceleration: 0.8,
     ground: 10,
     friction: 0.9,
-    time : 1,
-    fps: 1000/60,
+    time: 1,
+    fps: 1000 / 60,
     bgColor: 'rgba(247,247,247,1)',
     game_over: false
 }
@@ -36,25 +39,25 @@ let previous = Date.now();
 let now;
 let lag;
 
-let myInterval = setInterval( tick, 1000 / fps );
+let myInterval = setInterval(tick, 1000 / fps);
 
 function tick() {
-  now = Date.now();
-  elapsedTime = now - previous;
-  lag = elapsedTime - ( 1000 / fps );
-  dt.innerHTML = 'DeltaTime: ' + elapsedTime + ' ms';
-  lagInfo.innerHTML = 'Lag: ' + lag + ' ms';
-  previous = now;
+    now = Date.now();
+    elapsedTime = now - previous;
+    lag = elapsedTime - (1000 / fps);
+    dt.innerHTML = 'DeltaTime: ' + elapsedTime + ' ms';
+    lagInfo.innerHTML = 'Lag: ' + lag + ' ms';
+    previous = now;
 }
 
 // # -- Resolution -- # //
 
-function initializeResolution( w, h, color ) {
-    ctx.canvas.width = w;
-    ctx.canvas.height = h;
-    if( color !== null ) {
+function initializeResolution(width, height, color) {
+    ctx.canvas.width = width;
+    ctx.canvas.height = height;
+    if (color !== null) {
         ctx.fillStyle = color;
-        ctx.fillRect( 0, 0, w, h );
+        ctx.fillRect(0, 0, width, height);
         ctx.beginPath();
     }
 }
@@ -78,18 +81,19 @@ let controller = {
     keyListener(e) {
         let keyState = (e.type === 'keydown') ? true : false;
 
-        switch(e.keyCode) {
+        switch (e.keyCode) {
             case 37: // left key
                 controller.left = keyState;
-            break;
-
+                break;
             case 38: // up key
                 controller.up = keyState;
-            break;
-
+                break;
+            case 40: // down key
+                controller.down = keyState;
+                break;
             case 39: // right key
                 controller.right = keyState;
-            break;
+                break;
         }
     },
 };
@@ -100,181 +104,262 @@ window.addEventListener('keyup', controller.keyListener);
 // # -- Initialize resolution -- # //
 
 (function initialize() {
-    initializeResolution( 700, 250, game_settings.bgColor );
+    initializeResolution(700, 250, game_settings.bgColor);
 })();
 
 // # -- Enemies -- # //
 
 let enemies = [];
 
-function Enemy(x,y,w,h,color,timeout) {
-    this.x = x;
-    this.y = y;
-    this.width = w;
-    this.x_velocity = 5;
-    this.timeout = timeout;
-    this.height = h;
+function Enemy({
+    xCor,
+    yCor,
+    width,
+    height,
+    xVel,
+    color,
+}) {
+    this.xCor = xCor;
+    this.yCor = yCor;
+    this.width = width;
+    this.height = height;
+    this.xVel = xVel;
     this.color = color;
-    this.time = 0;
 }
 
-Enemy.prototype.draw = function() {
+Enemy.prototype.draw = function () {
     ctx.beginPath();
     ctx.strokeStyle = this.color;
-    ctx.strokeRect( this.x, this.y, this.width, this.height );
+    ctx.strokeRect(this.xCor, this.yCor, this.width, this.height);
 }
 
-Enemy.prototype.update = function() {
+Enemy.prototype.update = function () {
     this.draw();
-    this.time += 0.01;
-    this.x -= this.x_velocity ;
+    this.xCor -= this.xVel;
 }
 
+let enemyType = [{
+        type: 'one',
+        width: 60,
+        height: 30,
+        xCor: ctx.canvas.width - 15,
+        yCor: ctx.canvas.height - game_settings.ground,
+        xVel: 4,
+        color: 'rgba(87,87,87,1)'
+    },
+    {
+        type: 'two',
+        width: 30,
+        height: 60,
+        xCor: ctx.canvas.width - 15,
+        yCor: ctx.canvas.height - game_settings.ground,
+        xVel: 4,
+        color: 'rgba(87,87,87,1)'
+    },
+    {
+        type: 'three',
+        width: 30,
+        height: 30,
+        xCor: ctx.canvas.width - 15,
+        yCor: ctx.canvas.height - game_settings.ground,
+        xVel: 4,
+        color: 'rgba(87,87,87,1)'
+    },
+    {
+        type: 'bird',
+        width: 30,
+        height: 30,
+        xCor: ctx.canvas.width - 15,
+        yCor: ctx.canvas.height - game_settings.ground,
+        xVel: 4,
+        color: 'rgba(87,87,87,1)'
+    },
+];
+
+function updateEnemyData(arr, val) {
+    if(arr && val ) {
+        arr.forEach(el => {
+            el.xVel = val; 
+        });
+    }
+}
+
+enemyType.forEach(el => {
+    el.type !== 'bird' ? el.yCor -= el.height : el.yCor -= el.height * 2;
+});
+
+let spawn = {
+    interval: 2000,
+    currentCheckPoint: 50,
+    start() {
+        let spawn_interval = setInterval(() => {
+            enemies.push(new Enemy(enemyType[Math.floor(Math.random() * enemyType.length)]));
+        }, this.interval);
+    }
+};
+spawn.start();
+function checkScore() {
+    if (parseInt(game_settings.score, 10) > spawn.currentCheckPoint) {
+        spawn.currentCheckPoint += 100;
+        let currentxVel = enemyType[0]['xVel'];
+        currentxVel > 10 ? currentxVel : currentxVel += .5;
+        updateEnemyData(enemyType, currentxVel)
+        console.log(`reached ${spawn.currentCheckPoint} next checkpoint ${spawn.currentCheckPoint + 100} `)
+    }
+}
 // # -- Player -- # //
 
 let player = {
-    x: 50,
-    y: ctx.canvas.height / 2,
+    xCor: 50,
+    yCor: ctx.canvas.height / 2,
     width: 20,
     height: 40,
     color: 'rgba(87,87,87,1)',
-    x_velocity: 0,
-    y_velocity: 0,
+    xVel: 0,
+    yVel: 0,
     is_jumping: false,
     state: 'idle',
     draw() {
         ctx.beginPath();
         ctx.fillStyle = this.color;
-        ctx.fillRect( this.x, this.y, this.width, this.height );
+        ctx.fillRect(this.xCor, this.yCor, this.width, this.height);
     },
-    controls() {
-        this.y -= this.y_velocity;
+    log() {
+        state.innerHTML = 'State : ' + this.state;
+        player_y_position.innerHTML = 'Player yCor pos: ' + this.yCor;
+        player_y_position.innerHTML = 'Player yCor pos: ' + this.yCor;
+        score.innerHTML = 'Score ' + game_settings.score;
+    },
+    physics() {
+        this.yCor -= this.yVel;
+        this.yVel += game_settings.gravity; // adding gravity.
 
-        this.y_velocity += game_settings.gravity; // adding gravity.
-
-        if( this.y + this.height - this.y_velocity > ctx.canvas.height ) {
-            this.y = ctx.canvas.height - ( this.height + game_settings.ground );
+        if (this.yCor + this.height - this.yVel > ctx.canvas.height) {
+            this.yCor = ctx.canvas.height - (this.height + game_settings.ground);
         }
-        if( ( this.y + this.height) + game_settings.ground == ctx.canvas.height ) {
+        if ((this.yCor + this.height) + game_settings.ground == ctx.canvas.height) {
             this.state = 'idle';
         } else {
             this.state = 'jumping';
         }
-        if( controller.up && this.state == 'idle' ) {
-            this.y_velocity = 11;
+    },
+    controls() {
+        if (controller.up && this.state == 'idle') {
+            this.yVel = 13;
         }
+        if (controller.down) {
+            this.yVel += -3;
+            this.height = tools.lerp(this.height, 20, 0.5);
+            this.width = tools.lerp(this.width, 40, 0.5);
 
-        state.innerHTML = 'State : ' + this.state;
-        player_y_position.innerHTML = 'Player Y pos: ' + this.y;
-        player_y_position.innerHTML = 'Player Y pos: ' + this.y;
-        score.innerHTML = 'Score ' + game_settings.score;
+        } else {
+            this.width = tools.lerp(this.width, 20, 0.5);
+            this.height = tools.lerp(this.height, 40, 0.1);
+        }
     },
     update() {
         this.draw();
+        this.physics();
         this.controls();
+        this.log();
+
     }
 }
-
 
 // # -- Clear memory -- # //
 
 function clearMem() {
-    if( enemies.length >= 0 ) {
-        for( let i = 0; i < enemies.length; i++ ) {
-            if ( enemies[i].x + enemies[i].width < 0 ) {
-                enemies.splice( i , 1 );
+    if (enemies.length >= 0) {
+        for (let i = 0; i < enemies.length; i++) {
+            if (enemies[i].xCor + enemies[i].width < 0) {
+                enemies.splice(i, 1);
             }
-        } 
+        }
     }
 }
 
 // # -- Collision detection -- # //
 
 function isCollided() {
-    if(enemies.length>0) {
-        for( let i = 0; i < enemies.length; i++ ) {
-            if ( player.x + player.width >= enemies[i].x && enemies[i].x + enemies[i].width >= player.x && player.y + player.height >= enemies[i].y ) {
-                return {bool:true,enemy:enemies[i] };
+    if (enemies.length > 0) {
+        for (let i = 0; i < enemies.length; i++) {
+            if (player.xCor + player.width >= enemies[i].xCor && enemies[i].xCor + enemies[i].width >= player.xCor && player.yCor + player.height >= enemies[i].yCor) {
+                return {
+                    bool: false,
+                    enemy: enemies[i]
+                };
             } else {
-                return {bool:false,enemy:enemies[i]};
+                return {
+                    bool: false,
+                    enemy: enemies[i]
+                };
             }
-        } 
+        }
     }
 }
 
-let spawn = {
-    x: ctx.canvas.width - 15,
-    y: ctx.canvas.height - game_settings.ground - 30,
-    w: 30,
-    h: 30,
-    counter: 0,
-    color: 'rgba(87,87,87,1)',
-    start() {
-        setInterval(() => {
-            enemies.push( new Enemy(this.x, this.y, this.w, this.h, this.color) );
-        }, 1000)
-    }
-};
-spawn.start();
-
 // # -- Update -- # //
-let digits = [];
 
 (function update() {
     window.requestAnimationFrame(update);
-    ctx.clearRect(0,0,ctx.canvas.width,ctx.canvas.height);
+    ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
     world();
-    for( let i = 0; i < enemies.length; i++ ) {
+    for (let i = 0; i < enemies.length; i++) {
         enemies[i].update();
     }
     player.update();
     clearMem();
     gameManager();
+    checkScore()
 })();
 
-// Game Management //
 function gameManager() {
-    if(typeof isCollided() == 'object') {
-        let {bool,enemy,randRGBA} = isCollided();
-        if(bool) {
+    if (typeof isCollided() == 'object') {
+        let {
+            bool,
+            enemy,
+            randRGBA
+        } = isCollided();
+        if (bool) {
             enemy.color = tools.randRGBA();
             game_settings.game_over = true;
-            clearInterval(startScore)
+            clearInterval(startScore);
         } else {
             enemy.color = 'rgba(87,87,87,1)';
         }
     }
 }
+// Score //
 
+let digits = [];
 let len = 5;
-function Digit(i,len, currentVal) {
+
+function Digit(i, len, currentVal) {
     this.i = i;
     this.currentVal = currentVal;
     this.len = len;
 }
-Digit.prototype.update = function() {
-        let previousDigit = this.i + 1 > this.len ? null : digits[this.i+1];
-        if(!previousDigit) {
-            this.currentVal++;
-        } else if(previousDigit.currentVal > 8) {
-            this.currentVal++;
-            previousDigit.currentVal = 0;
-        }
-
-        let values = [];
-        for (i=0; i<digits.length;i++) {
-            values.push(digits[i].currentVal);
-            
-        }
-        game_settings.score = values.toString().replace(/,/g,"");
+Digit.prototype.update = function () {
+    let previousDigit = this.i + 1 > this.len ? null : digits[this.i + 1];
+    if (!previousDigit) {
+        this.currentVal++;
+    } else if (previousDigit.currentVal > 8) {
+        this.currentVal++;
+        previousDigit.currentVal = 0;
+    }
+    let values = [];
+    for (i = 0; i < digits.length; i++) {
+        values.push(digits[i].currentVal);
+    }
+    game_settings.score = values.toString().replace(/,/g, "");
 }
 
-for(let i=0; i<len; i++) {
-    digits.push(new Digit(i,len,0))
+for (let i = 0; i < len; i++) {
+    digits.push(new Digit(i, len, 0))
 }
 
 let startScore = setInterval(() => {
-    for(let i=0; i<digits.length; i++) {
+    for (let i = 0; i < digits.length; i++) {
         digits[i].update();
     }
 }, 100)
